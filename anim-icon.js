d69a4b1af11907417245c26a697ab5ca46b1bdb9
Lukas Rimkus <lukas.rimkus@procemex.com>
2019-07-10 17:56:18 +0300

    adding files

---
+++ anim-icon.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".lukasiju-mnu").click(function() {
+        var a = $(this).toggleClass("activated");
+        var b = $("nav").toggleClass("nav-primary");
+    });
+    return a , b;
+    });
+ 
+
+  
+  
+  
