565883bde09c1fffa3b2f0569cd5e8eb9e597fe5
Lukasijus <lukasrimkuslg@Gmail.com>
2019-07-13 17:40:34 +0300

    update index.php

--- index.php
+++ index.php
@@ -4,12 +4,19 @@
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-menu-icon.css">
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-menu-table.css">
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-header-style.css">
+        <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-menu-navigation-bar.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script type="text/javascript" src="lukasijusweb/anim-icon.js"></script>
+        <script type="text/javascript" src="lukasijusweb/sanim-icon.js"></script>
    
     </head>
 <body>
 <header>
+<ul>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#CV">Computer Vision</a></li>
+                    <li><a href="#WD">Web Development</a></li>
+                    <li><a href="#about">About</a></li>
+                  </ul>
             <button id = "btn" class="lukasiju-mnu" aria-expanded="false" aria-pressed="false"><span></span></button>
             <nav >
             </nav>
