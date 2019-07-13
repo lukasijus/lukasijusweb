75c5c2033294bdbeb1146ad19f73657073ae87c9
Lukasijus <lukasrimkuslg@Gmail.com>
2019-07-13 18:07:55 +0300

    update index

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-menu-navigation-bar.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script type="text/javascript" src="lukasijusweb/anim-icon.js"></script>
-   
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 <body>
 <header>
