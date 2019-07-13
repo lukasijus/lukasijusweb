275daa60b5908bec902403ed50143d109b17a1f8
Lukasijus <lukasrimkuslg@Gmail.com>
2019-07-13 17:43:08 +0300

    update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-header-style.css">
         <link rel="stylesheet" type = "text/css" href="lukasijusweb/lukasiju-menu-navigation-bar.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-        <script type="text/javascript" src="lukasijusweb/sanim-icon.js"></script>
+        <script type="text/javascript" src="lukasijusweb/anim-icon.js"></script>
    
     </head>
 <body>
