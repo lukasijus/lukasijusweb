3170cb17f4b3163d717610157acc522f481a74b7
Lukasijus <lukasrimkuslg@Gmail.com>
2019-07-22 00:19:17 +0300

    update page_title.js

--- page_title.js
+++ page_title.js
@@ -1,9 +1,9 @@
 window.onload = function getName() {
     var x = document.title;
     name = document.getElementById("name");
-    if (x === 'Lukas Rimkus | UX/UI Designer & Full Stack Engineer') {
-        document.getElementById("name").innerHTML = 'UX/UI Designer & Full Stack Engineer';
+    if (x === "Lukas Rimkus | UX/UI Designer & Full Stack Engineer") {
+        document.getElementById("name").innerHTML = "UX/UI Designer & Full Stack Engineer";
     }
     else {
         document.getElementById("name").innerHTML = document.title;}
-}
+};
