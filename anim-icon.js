babd7a0f771e2cd0204e33976ce13375337058f1
Lukasijus <lukasrimkuslg@Gmail.com>
2019-08-02 12:07:35 +0200

    update latest

--- anim-icon.js
+++ anim-icon.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-    $(".lukasiju-mnu").click(function() {
-        var a = $(this).toggleClass("activated");
-        var b = $("nav").toggleClass("nav-primary");
-    });
-    return a , b;
-    });
- 
-
-  
-  
-  
+  $(".lukasiju-mnu").click(function() {
+    var a = $(this).toggleClass("activated");
+    var b = $("nav").toggleClass("nav-primary");
+  });
+  return a, b;
+});
--- moving_text.js
+++ moving_text.js
@@ -1,17 +1,16 @@
 $(function() {
-  
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-        if (scroll >= 50) {
-            scroll2 = scroll - 49;
-            document.getElementById("text1").style.transform = "translateX(" + scroll2 + "px)";
-        
-        } 
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      scroll2 = scroll - 49;
+      document.getElementById("text1").style.transform =
+        "translateX(" + scroll2 + "px)";
+    }
 
-        if (scroll >=700) {
-            scroll3 = scroll - 699;
-            document.getElementById("text2").style.transform = "translateX(" + scroll3 + "px)";
-        }
-    });
-  
+    if (scroll >= 700) {
+      scroll3 = scroll - 699;
+      document.getElementById("text2").style.transform =
+        "translateX(" + scroll3 + "px)";
+    }
+  });
 });
